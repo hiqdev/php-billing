027e5b9edf8c901fc293243752323c5c708c26cb
Andrii Vasyliev <sol@hiqdev.com>
2017-06-06 18:56:43 +0000

    added exceptions

---
+++ src/ExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\php\billing;
+
+/**
+ * Common interface for all exceptions thrown by this library.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+interface ExceptionInterface
+{
+}
