6e3cc3aa42cb7c4503d2d24f23117bcb90c31af8
Andrii Vasyliev <sol@hiqdev.com>
2017-11-25 15:04:44 +0000

    added `SaleFactory`

---
+++ src/sale/SaleFactory.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\sale;
+
+/**
+ * Default sale factory.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class SaleFactory implements SaleFactoryInterface
+{
+    /**
+     * Creates sale object.
+     * @return Sale
+     */
+    public function create(SaleCreationDto $dto)
+    {
+        return new Sale($dto->id, $dto->target, $dto->customer, $dto->plan, $dto->time);
+    }
+}
---
+++ src/sale/SaleFactoryInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\sale;
+
+/**
+ * Sale factory interface.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+interface SaleFactoryInterface
+{
+    /**
+     * Creates sale object.
+     * @return Sale
+     */
+    public function create(SaleCreationDto $dto);
+}
