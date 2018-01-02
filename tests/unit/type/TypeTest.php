fd1dbe4eab79be391d3f6e313b5902fd98d24024
Andrii Vasyliev <sol@hiqdev.com>
2018-01-02 15:50:01 +0000

    added tests for `Type::matches()`

--- tests/unit/type/TypeTest.php
+++ tests/unit/type/TypeTest.php
@@ -18,39 +18,52 @@ use hiqdev\php\billing\type\TypeInterface;
  */
 class TypeTest extends \PHPUnit\Framework\TestCase
 {
-    protected $id1 = 1;
+    protected $sid1 = 101;
+    protected $sid2 = 102;
 
-    protected $id2 = 2;
+    protected $sop1 = 'server1';
+    protected $sop2 = 'server2';
 
-    protected $sr1 = 'server1';
+    protected $did1 = 201;
+    protected $did2 = 202;
 
-    protected $sr2 = 'server2';
+    protected $dop1 = 'domain1';
+    protected $dop2 = 'domain2';
 
     protected function setUp()
     {
-        $this->server11 = new Type($this->id1,  $this->sr1);
-        $this->server_1 = new Type(null,        $this->sr1);
-        $this->server1_ = new Type($this->id1,  null);
-        $this->server22 = new Type($this->id2,  $this->sr2);
-        $this->server_2 = new Type(null,        $this->sr2);
-        $this->server2_ = new Type($this->id2,  null);
+        $this->server11 = new Type($this->sid1,  $this->sop1);
+        $this->server_1 = new Type(null,         $this->sop1);
+        $this->server1_ = new Type($this->sid1,  null);
+        $this->server22 = new Type($this->sid2,  $this->sop2);
+        $this->server_2 = new Type(null,         $this->sop2);
+        $this->server2_ = new Type($this->sid2,  null);
 
-        $this->domain1 = new Type($this->id1, 'domain1');
-        $this->domain2 = new Type($this->id2, 'domain2');
+        $this->domain11 = new Type($this->did1,  $this->dop1);
+        $this->domain_1 = new Type(null,         $this->dop1);
+        $this->domain1_ = new Type($this->did1,  null);
+        $this->domain22 = new Type($this->did2,  $this->dop2);
+        $this->domain_2 = new Type(null,         $this->dop2);
+        $this->domain2_ = new Type($this->did2,  null);
     }
 
     protected function tearDown()
     {
     }
 
+    protected function copy(TypeInterface $type)
+    {
+        return new Type($type->getId(), $type->getName());
+    }
+
     public function testGetUniqueId()
     {
-        $this->assertSame($this->id1,   $this->server11->getUniqueId());
-        $this->assertSame($this->sr1,   $this->server_1->getUniqueId());
-        $this->assertSame($this->id1,   $this->server1_->getUniqueId());
-        $this->assertSame($this->id2,   $this->server22->getUniqueId());
-        $this->assertSame($this->sr2,   $this->server_2->getUniqueId());
-        $this->assertSame($this->id2,   $this->server2_->getUniqueId());
+        $this->assertSame($this->sid1,  $this->server11->getUniqueId());
+        $this->assertSame($this->sop1,  $this->server_1->getUniqueId());
+        $this->assertSame($this->sid1,  $this->server1_->getUniqueId());
+        $this->assertSame($this->sid2,  $this->server22->getUniqueId());
+        $this->assertSame($this->sop2,  $this->server_2->getUniqueId());
+        $this->assertSame($this->sid2,  $this->server2_->getUniqueId());
     }
 
     public function testEquals()
@@ -58,12 +71,13 @@ class TypeTest extends \PHPUnit\Framework\TestCase
         $all = [$this->server11, $this->server_1, $this->server1_];
         $copies = [];
         foreach ($all as $k => $v) {
-            $copies[$k] = new Type($v->getId(), $v->getName());
+            $copies[$k] = $this->copy($v);
         }
 
         foreach ($all as $k => $v) {
             foreach ($copies as $j => $w) {
                 $this->checkEquals($j === $k, $v, $w);
+                $this->checkEquals($j === $k, $w, $v);
             }
         }
     }
@@ -77,4 +91,42 @@ class TypeTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($expect, $check);
     }
 
+    public function testMatches()
+    {
+        $this->checkMatches([
+            $this->server11, $this->copy($this->server11),
+            $this->server_1, $this->copy($this->server_1),
+        ]);
+        $this->checkMatches([
+            $this->server11, $this->copy($this->server11),
+            $this->server1_, $this->copy($this->server1_),
+        ]);
+        $this->checkDoesntMatch([
+            $this->server_1, $this->server1_, $this->server_2, $this->server2_,
+            $this->domain_1, $this->domain1_, $this->domain_2, $this->domain2_,
+        ]);
+    }
+
+    protected function checkDoesntMatch(array $types)
+    {
+        $this->checkMatches($types, false);
+    }
+
+    protected function checkMatches(array $types, bool $expect = true)
+    {
+        foreach ($types as $k => $v) {
+            foreach ($types as $j => $w) {
+                $this->checkSingleMatch($k === $j || $expect, $v, $w);
+            }
+        }
+    }
+
+    protected function checkSingleMatch(bool $expect, $lhs, $rhs)
+    {
+        $check = $lhs->matches($rhs);
+        if ($check !== $expect) {
+            var_dump('no match', $expect, $lhs, $rhs);
+        }
+        $this->assertSame($expect, $check);
+    }
 }
