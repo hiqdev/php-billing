e453cfcb8b1664554ee9559cb19a4eb7b52d1fec
Andrii Vasyliev <sol@hiqdev.com>
2018-01-23 17:32:17 +0000

    added `SaleCreationDto`

---
+++ src/sale/SaleCreationDto.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\sale;
+
+/**
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class SaleCreationDto
+{
+    public $id;
+
+    public $target;
+
+    public $customer;
+
+    public $plan;
+
+    public $time;
+}
