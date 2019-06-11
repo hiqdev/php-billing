2a7b17490062d75b4b0f6020dba22d461f1a5fd8
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2019-06-11 11:58:32 +0300

    Added Action multiplexer interface

---
+++ src/action/mux/ActionMuxInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace hiqdev\php\billing\action\mux;
+
+use hiqdev\php\billing\action\ActionInterface;
+
+/**
+ * Interface ActionMuxInterface provides an API to multiplex/demultiplex actions.
+ *
+ * Demultiplexing might be useful to simplify public API by passing a single action into the billing,
+ * that actually represents a set of more specific action.
+ *
+ * For example, action "purchase a small gift in box" can be actually represented with a set of
+ * the following specific actions, that should be charged:
+ *
+ * - purchase 1 box
+ * - purchase 1 small gift
+ * - purchase 1 set of packing
+ * - purchase a box packing service
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+interface ActionMuxInterface
+{
+    /**
+     * Takes the $action and returns the array of demultiplexed actions.
+     *
+     * @param ActionInterface $action The original action
+     * @return ActionInterface[] Array of demultiplexed actions
+     */
+    public function demultiplex(ActionInterface $action): array;
+
+    /**
+     * Takes array of actions and returns array of multiplexed actions.
+     *
+     * Method can return multiple actions, if the input set can not be multiplex to a single action.
+     *
+     * If actions can not be multiplexed, input array MUST be returned without any changes.
+     *
+     * @param ActionInterface[] $actions
+     * @return ActionInterface[]
+     */
+    public function multiplex(array $actions): array;
+}
---
+++ src/action/mux/MuxException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace hiqdev\php\billing\action\mux;
+
+use Exception;
+use hiqdev\php\billing\ExceptionInterface;
+
+/**
+ * Class MuxException
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+class MuxException extends Exception implements ExceptionInterface
+{
+}
