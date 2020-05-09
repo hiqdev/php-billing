574e695ad2a681548e405686af7af6029fe80dd4
Andrii Vasyliev <sol@hiqdev.com>
2020-05-09 11:16:28 +0000

    csfixed header comments

--- src/charge/derivative/ChargeDerivativeQueryInterface.php
+++ src/charge/derivative/ChargeDerivativeQueryInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
 
 namespace hiqdev\php\billing\charge\derivative;
 
--- src/event/EventAwareTrait.php
+++ src/event/EventAwareTrait.php
@@ -1,6 +1,14 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
 
 namespace hiqdev\php\billing\event;
 
