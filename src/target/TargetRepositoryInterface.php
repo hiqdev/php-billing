3d1abea8b7a2b1a14e926bc594308006c98fe6e0
Andrii Vasyliev <sol@hiqdev.com>
2020-05-15 13:14:41 +0000

    Add `TargetRepositoryInterface`

---
+++ src/target/TargetRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\target;
+
+/**
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+interface TargetRepositoryInterface
+{
+    public function save(TargetInterface $target);
+}
