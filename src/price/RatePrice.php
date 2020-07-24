d1a5f2963d5dfd07ca4dfef2c8095902670f147f
Andrii Vasyliev <sol@hiqdev.com>
2020-07-24 13:40:11 +0000

    Change `RatePrice` sum calculattion for consumption to be positive dollars

--- src/price/RatePrice.php
+++ src/price/RatePrice.php
@@ -47,7 +47,7 @@ class RatePrice extends AbstractPrice
 
     public function calculateSum(QuantityInterface $quantity): ?Money
     {
-        $sum = $quantity->multiply(-$this->rate/100.0);
+        $sum = $quantity->multiply(-$this->rate);
         $currency = strtoupper($sum->getUnit()->getName());
 
         return Money::{$currency}(floor($sum->getQuantity()));
