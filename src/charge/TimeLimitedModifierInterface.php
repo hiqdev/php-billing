dd7d94fcdd293c17964a62ab3baba5279a3f99eb
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2020-10-29 18:32:49 +0200

    Added FullCombination::toPlainModifiersArray, refactored Modifier

---
+++ src/charge/TimeLimitedModifierInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\charge;
+
+use DateTimeImmutable;
+use hiqdev\php\billing\formula\FormulaRuntimeError;
+
+interface TimeLimitedModifierInterface
+{
+    /**
+     * @param DateTimeImmutable $time
+     * @return bool whether the modifier is applicable at the passed $time
+     * @throws FormulaRuntimeError when modifier is misconfigured
+     */
+    public function checkPeriod(DateTimeImmutable $time): bool;
+}
