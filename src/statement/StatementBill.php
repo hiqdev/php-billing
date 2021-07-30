ec073d95ce3f3583a714d2e15ed2829672009a2c
Yurii Myronchuk <bladeroot@gmail.com>
2021-07-30 09:41:01 +0000

    add extenssions for Bill

---
+++ src/statement/StatementBill.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * PHP Billing Library
+ *
+ * @link      https://github.com/hiqdev/php-billing
+ * @package   php-billing
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\billing\statement;
+
+use hiqdev\php\billing\bill\Bill;
+use hiqdev\php\billing\bill\BillInterface;
+use hiqdev\php\billing\bill\BillState;
+use hiqdev\php\billing\type\TypeInterface;
+use hiqdev\php\billing\customer\CustomerInterface;
+use hiqdev\php\billing\target\TargetInterface;
+use hiqdev\php\billing\plan\PlanInterface;
+use hiqdev\php\units\QuantityInterface;
+use DateTimeImmutable;
+use Money\Money;
+
+
+
+/**
+ * Bill.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class StatementBill extends Bill implements BillInterface
+{
+    /** @var DateTimeImmutable */
+    protected $month;
+
+    /** @var string */
+    protected $from
+
+    public function __construct(
+        $id,
+        TypeInterface $type,
+        DateTimeImmutable $time,
+        Money $sum,
+        QuantityInterface $quantity,
+        CustomerInterface $customer,
+        TargetInterface $target = null,
+        PlanInterface $plan = null,
+        array $charges = [],
+        BillState $state = null,
+        DateTimeImmutable $month,
+        ?string $from = null
+    ) {
+        parent::__construct(
+            $id,
+            $type,
+            $time,
+            $sum,
+            $quantity,
+            $customer,
+            $target,
+            $plan,
+            $charges,
+            $state
+        );
+        $this->month        = $month;
+        $this->from         = $from;
+    }
+
+    public function getMonth(): DateTimeImmutable
+    {
+        return $this->month;
+    }
+
+    public function getFrom(): string
+    {
+        return $this->from ?? '';
+    }
+}
