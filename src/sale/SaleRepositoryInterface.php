9b659ac62a097114563a5232e46ab57a10da24ba
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-10-28 15:19:42 +0200

    HP-2139 tiny

--- src/sale/SaleRepositoryInterface.php
+++ src/sale/SaleRepositoryInterface.php
@@ -42,8 +42,4 @@ interface SaleRepositoryInterface
      * Find all active sales at given time
      */
     public function findAllActive(Specification $specification, ?DateTimeImmutable $time): array;
-
-    public function isTariffInUse(int $tariffId): bool;
-
-    public function deleteByTariffId(int $tariffId): void;
 }
