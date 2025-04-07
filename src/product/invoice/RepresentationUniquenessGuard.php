29218ec5aaaa14fb60471cd83afab35ac28c519e
Vadym Hrechukha <vadimgrechuha@gmail.com>
2025-04-07 23:38:21 +0300

    HP-1751 fixed RepresentationUniquenessGuard class

--- src/product/invoice/RepresentationUniquenessGuard.php
+++ src/product/invoice/RepresentationUniquenessGuard.php
@@ -4,22 +4,23 @@ namespace hiqdev\php\billing\product\invoice;
 
 class RepresentationUniquenessGuard
 {
-    private array $keys = [];
+    private static array $keys = [];
 
     public function ensureUnique(RepresentationInterface $representation): void
     {
         $key = $this->generateKey($representation);
 
-        if (isset($this->keys[$key])) {
+        if (isset(self::$keys[$key])) {
             throw new DuplicateRepresentationException("Duplicate '$key' representation");
         }
 
-        $this->keys[$key] = true;
+        self::$keys[$key] = true;
     }
 
     private function generateKey(RepresentationInterface $representation): string
     {
         $reflect = new \ReflectionClass($representation);
+
         return $reflect->getShortName() . ':' . $representation->getType()->getName();
     }
 }
