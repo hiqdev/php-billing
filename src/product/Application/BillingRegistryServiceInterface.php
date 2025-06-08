48f33beb52769aa207dcf736dd887cdcee1d41ac
Vadym Hrechukha <vadimgrechuha@gmail.com>
2025-06-08 10:27:38 +0300

    HP-2512 removed the next interfaces because this does not make any sense and only confusing - BillingRegistryBehaviorServiceInterface - BillingRegistryTariffServiceInterface

--- src/product/Application/BillingRegistryServiceInterface.php
+++ src/product/Application/BillingRegistryServiceInterface.php
@@ -2,12 +2,19 @@
 
 namespace hiqdev\php\billing\product\Application;
 
+use Generator;
 use hiqdev\php\billing\product\AggregateInterface;
+use hiqdev\php\billing\product\behavior\BehaviorInterface;
+use hiqdev\php\billing\product\behavior\BehaviorNotFoundException;
+use hiqdev\php\billing\product\behavior\InvalidBehaviorException;
+use hiqdev\php\billing\product\Exception\TariffTypeDefinitionNotFoundException;
 use hiqdev\php\billing\product\invoice\RepresentationInterface;
+use hiqdev\php\billing\product\price\PriceTypeDefinitionInterface;
 use hiqdev\php\billing\product\quantity\FractionQuantityData;
 use hiqdev\php\billing\product\quantity\QuantityFormatterInterface;
+use hiqdev\php\billing\product\TariffTypeDefinitionInterface;
 
-interface BillingRegistryServiceInterface extends BillingRegistryTariffServiceInterface, BillingRegistryBehaviorServiceInterface
+interface BillingRegistryServiceInterface
 {
     /**
      * @param string $representationClass
@@ -18,4 +25,37 @@ interface BillingRegistryServiceInterface extends BillingRegistryTariffServiceIn
     public function createQuantityFormatter(string $type, FractionQuantityData $data): QuantityFormatterInterface;
 
     public function getAggregate(string $type): AggregateInterface;
+
+    /**
+     * @param string $tariffName
+     * @return TariffTypeDefinitionInterface
+     * @throws TariffTypeDefinitionNotFoundException
+     */
+    public function getTariffTypeDefinitionByName(string $tariffName): TariffTypeDefinitionInterface;
+
+    /**
+     * @param string $type - full type like 'overuse,lb_capacity_unit'
+     * @param string $behaviorClassWrapper
+     * @return BehaviorInterface
+     * @throws BehaviorNotFoundException
+     * @throws InvalidBehaviorException
+     */
+    public function getBehavior(string $type, string $behaviorClassWrapper): BehaviorInterface;
+
+
+    /**
+     * Find all behaviors attached to any TariffType or PriceType by specified Behavior class.
+     *
+     * @param string $behaviorClassWrapper
+     * @return Generator<BehaviorInterface>
+     */
+    public function getBehaviors(string $behaviorClassWrapper): Generator;
+
+    /**
+     * Find all PriceTypeDefinition in registry by specified Behavior class.
+     *
+     * @param string $behaviorClassWrapper
+     * @return Generator<PriceTypeDefinitionInterface>
+     */
+    public function findPriceTypeDefinitionsByBehavior(string $behaviorClassWrapper): Generator;
 }
