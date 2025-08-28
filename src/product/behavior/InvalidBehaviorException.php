e28267ac15f5e32f76910f773e126ab2e9d90bf8
Vadym <114911409+VadymHrechukha@users.noreply.github.com>
2025-08-28 15:43:14 +0300

    HP-2676: Fix issues caused by HP-2581 release: tariff view error, failing CI jobs, and SinglePrice type error (#105)

--- src/product/behavior/InvalidBehaviorException.php
+++ src/product/behavior/InvalidBehaviorException.php
@@ -2,8 +2,11 @@
 
 namespace hiqdev\php\billing\product\behavior;
 
+use hidev\exception\HasContext;
+use hidev\exception\HasContextInterface;
 use InvalidArgumentException;
 
-class InvalidBehaviorException extends InvalidArgumentException
+class InvalidBehaviorException extends InvalidArgumentException implements HasContextInterface
 {
+    use HasContext;
 }
